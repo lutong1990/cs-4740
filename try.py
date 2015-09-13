d56d8c45947f18ca64aa21a908decb12ebd3c698
Tong <tl642@cornell.edu>
2015-09-13 16:43:42 -0400

    finish probability

--- try.py
+++ try.py
@@ -4,6 +4,7 @@ import os
 import nltk
 import sys
 from sets import Set
+import numpy as np
 
 def bigram_count(filename):
     Bigram = {}
@@ -23,15 +24,16 @@ def bigram_count(filename):
                             Bigram[word][bigram] += 1
                         else:
                             Bigram[word][bigram] = 1
+
+#calculating the probabilities   
+    for key,value in Bigram.iteritems():
+        n = sum(Bigram[key].values())
+        for key2,value2 in Bigram[key].iteritems():
+            Bigram[key][key2] = float(value2)/n
     return Bigram
-''' 
-    calculating the probabilities   
-    for i,j in Bigram:
-        n = len(Bigram[i])
-        Bigram[i][j] = Bigram[i][j]/n
-'''        
+
 
 if __name__ == '__main__':
     word_count_dict = bigram_count('/home/tong/Documents/CS5740/Project1/train_books/children/the_junior_classics_vol_1.txt')
 
-print word_count_dict['a']
+print word_count_dict['a']['a fire']
