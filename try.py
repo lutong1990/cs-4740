4292c4dce2ebf86dfd66fa45b25aa437f6ca098f
Tong <tl642@cornell.edu>
2015-09-14 21:54:24 -0400

    added find_next_word

--- try.py
+++ try.py
@@ -19,7 +19,7 @@ def bigram_count(filename):
                         if word not in wordSet:
                             wordSet.add(word)    
                             Bigram[word] = {}    
-                        bigram = word +' '+ words[i+1]        
+                        bigram = words[i+1]        
                         if bigram in Bigram[word]:
                             Bigram[word][bigram] += 1
                         else:
@@ -30,10 +30,31 @@ def bigram_count(filename):
         n = sum(Bigram[key].values())
         for key2,value2 in Bigram[key].iteritems():
             Bigram[key][key2] = float(value2)/n
+            
     return Bigram
 
 
 if __name__ == '__main__':
     word_count_dict = bigram_count('/home/tong/Documents/CS5740/Project1/train_books/children/the_junior_classics_vol_1.txt')
 
-print word_count_dict['a']['a fire']
+
+def find_next_word(word):
+	'''
+	Here I use multinomial distribution to model the word generation
+
+	A little background for multinomial distribution:
+		it is a generlization of binomial distribution, in a way that the number of possible outcomes is greater than two 
+
+	Use of the function:
+		input a word -> it will use the probability of all words after it in the corpora, to randomly select a following word
+						-> output the following word
+
+	test of the function:
+		run codes several times, change 'new' to something else, and run it multiple times again, you will see how the outputs change
+	'''
+	next_word = np.random.multinomial(1,word_count_dict[word].values(),1)
+	index = np.transpose(np.nonzero(next_word))[:,1]
+	return word_count_dict[word].keys()[index]
+
+
+print find_next_word('new')
